0769393d483be435615921d83b9d1f23fd886c4d
Jerry Ferreira <57259731+tomf1br@users.noreply.github.com>
2020-08-20 20:09:53 -0300

    aula09

---
+++ aula09/JogoDado.java
@@ -0,0 +1,52 @@
+package exercicios;
+
+public class JogoDado {
+
+	public static void main(String[] args) {
+		int dado = (int) (Math.random() * 6 + 1);
+		switch (dado) {
+		case 1:
+			System.out.println("╔═══════╗");
+			System.out.println("║       ║");
+			System.out.println("║   ○   ║");
+			System.out.println("║       ║");
+			System.out.println("╚═══════╝");
+			break;
+		case 2:
+			System.out.println("╔═══════╗");
+			System.out.println("║ ○     ║ ");
+			System.out.println("║       ║");
+			System.out.println("║     ○ ║");
+			System.out.println("╚═══════╝");
+			break;
+		case 3:
+			System.out.println("╔═══════╗");
+			System.out.println("║ ○     ║ ");
+			System.out.println("║   ○   ║");
+			System.out.println("║     ○ ║");
+			System.out.println("╚═══════╝");
+			break;
+		case 4:
+			System.out.println("╔═══════╗");
+			System.out.println("║ ○   ○ ║ ");
+			System.out.println("║       ║");
+			System.out.println("║ ○   ○ ║");
+			System.out.println("╚═══════╝");
+			break;
+		case 5:
+			System.out.println("╔═══════╗");
+			System.out.println("║ ○   ○ ║ ");
+			System.out.println("║   ○   ║");
+			System.out.println("║ ○   ○ ║");
+			System.out.println("╚═══════╝");
+			break;
+		case 6:
+			System.out.println("╔═══════╗");
+			System.out.println("║ ○   ○ ║");
+			System.out.println("║ ○   ○ ║");
+			System.out.println("║ ○   ○ ║");
+			System.out.println("╚═══════╝");
+			break;
+		}
+	}
+}
